f9a5166cb189f439d4597f15b30e21698d89d1be
Natnael-Gedlu <nategd800@gmail.com>
2024-04-09 16:08:10 +0300

    added module-level variable named babel

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -12,8 +12,6 @@ class Config(object):
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"app.config.from_object(Config)
-babel = Babel(app)
-
 
 
 app = Flask(__name__)
