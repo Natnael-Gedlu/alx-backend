78ec7accd42c27ba9856fb82fd581709fe2d87ce
Natnael-Gedlu <nategd800@gmail.com>
2024-04-09 17:18:09 +0300

    Force locale with URL parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -35,8 +35,8 @@ def get_locale():
     """
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
+        print(locale)
         return locale
-
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
