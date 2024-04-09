202c80758218df74d2ac4edb5965e184c8d2c9ae
Natnael-Gedlu <nategd800@gmail.com>
2024-04-09 16:29:54 +0300

    added locale selector for the Babel extension

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+A basic Flask app
+"""
+from flask_babel import Babel
+from flask import Flask, render_template
+
+
+class Config(object):
+    """
+    Configuration for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Route to index
+    """
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """
+    A locale selector for the Babel extension in Flask
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
