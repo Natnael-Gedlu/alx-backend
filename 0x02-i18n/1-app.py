2f8b4b8d619b1126886d32ca576620e17eae8c26
Natnael-Gedlu <nategd800@gmail.com>
2024-04-09 16:04:50 +0300

    added module-level variable named babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+A basic Flask app
+"""
+from flask_babel import Babel
+from flask import Flask, render_template
+
+class Config(object):
+    """
+    Configuration for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"app.config.from_object(Config)
+babel = Babel(app)
+
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Route to index
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
