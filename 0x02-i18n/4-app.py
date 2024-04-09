4b357a39ede7421882eb6299c8802f6e46e39da3
Natnael-Gedlu <nategd800@gmail.com>
2024-04-09 17:21:03 +0300

    fixed code output

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -37,7 +37,6 @@ def get_locale():
     if locale in app.config['LANGUAGES']:
         print(locale)
         return locale
-
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
